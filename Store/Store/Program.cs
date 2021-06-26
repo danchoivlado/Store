708ee2e6c2bb6f6c93dd379bd405b1b0223c7064
Йордан Йорданов <dani_yordani@abv.bg>
2021-06-26 21:14:56 +0300

    Store

---
+++ Store/Store/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Store.Core;
+
+namespace Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testEngine = new TestEngine();
+            testEngine.Run();
+        }
+    }
+}
